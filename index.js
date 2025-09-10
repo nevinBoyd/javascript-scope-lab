cd37ede50a6b059b223c55d73df82c655bb728ce
Nevin <n.boyd1@yahoo.com>
2025-09-10 08:32:59 -0700

    Fixed grammatical errors, fixed syntax for addBurger, originally had 'let' instead of 'function'

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file
+
+const burgers = ['Hamburger', 'Cheeseburger']
+let featuredDrink = 'Strawberry Milkshake'
+
+function addBurger() {
+    let newBurger = 'Flatburger'
+    burgers.push(newBurger)
+}
+
+if (true) {
+    anotherNewBurger = 'Maple Bacon Burger'
+    burgers.push(anotherNewBurger)
+
+    function changeFeaturedDrink() {
+        featuredDrink = 'The JavaShake'
+    }
+}
